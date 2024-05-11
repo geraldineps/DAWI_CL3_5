2bc916a8ee608476d0cc977b23be04ec6a1a43ed
graldineps <geriifujioka@gmail.com>
2024-05-11 02:19:31 -0500

    Pregunta 2 terminada

--- src/app/pregunta-2/pregunta-2.component.ts
+++ src/app/pregunta-2/pregunta-2.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pregunta-2',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pregunta-2.component.html',
   styleUrl: './pregunta-2.component.scss'
 })
-export class Pregunta2Component {
+export class Pregunta2Component {  
+
+    numeros= {
+        numero1: 0,
+        numero2: 0,
+        numero3: 0,
+        numero4: 0,
+        numero5: 0,
+        resultado: 0
+      }
+
+      mostrarResultado = false;
+  
+    calcularMayor() {
+      const mayor = [Number(this.numeros.numero1),Number(this.numeros.numero2),Number(this.numeros.numero3),Number(this.numeros.numero4),Number(this.numeros.numero5)];
+      this.numeros.resultado = Math.max(...mayor);
+      this.mostrarResultado = true ;
+    }
 
 }
